b259a1c79cedf4b942a1c07e0a32b9fcb326543f
164 <164@DESKTOP-4R0ABGR>
2019-05-22 22:03:24 +0100

    Versión 2
    Se ha añadido otro jugador al juego. Ahora hay 3.

--- JuegoAdivinarNum/src/JuegoAdivinar.java
+++ JuegoAdivinarNum/src/JuegoAdivinar.java
@@ -1,38 +1,49 @@
 public class JuegoAdivinar {
     Player p1;
     Player p2;
+    Player p3;
     public void startGame() {
         p1 = new Player();
         p2 = new Player();
+        p3 = new Player();
         int adivinarp1 = 0;
         int adivinarp2 = 0;
+        int adivinarp3 = 0;
         boolean p1isRight = false;
         boolean p2isRight = false;
+        boolean p3isRight = false;
         int numero = (int)(Math.random() * 10);
         System.out.println("Estoy pensando en un número entre 0 y 9...");
         while (true) {
-            System.out.println("El número que hay que adivinar es " + numero);
+            System.out.println("Número que debes adivinar es " + numero);
             p1.adivinar();
             p2.adivinar();
+            p3.adivinar();
             adivinarp1 = p1.number;
             System.out.println("Player 1 adivinó " + adivinarp1);
             adivinarp2 = p2.number;
             System.out.println("Player 2 adivinó " + adivinarp2);
+            adivinarp3 = p3.number;
+            System.out.println("Player 3 adivinó " + adivinarp3);
             if (adivinarp1 == numero) {
                 p1isRight = true;
             }
             if (adivinarp2 == numero) {
                 p2isRight = true;
             }
-            if (p1isRight || p2isRight) {
+            if (adivinarp3 == numero) {
+                p3isRight = true;
+            }
+            if (p1isRight || p2isRight || p3isRight) {
                 System.out.println("Tenemos un ganador!");
                 System.out.println("Player 1 acertó? " + p1isRight);
                 System.out.println("Player 2 acertó? " + p2isRight);
+                System.out.println("Player 3 acertó? " + p3isRight);
                 System.out.println("Game is over.");
                 break; // juego acabado, asi que se sale del loop
             } else {
                 // seguimos porque nadie acertó
-                System.out.println("Hay que intentarlo de nuevo");
+                System.out.println("Players will have to try again.");
             } // end if/else
         } // end loop
     } // end method
